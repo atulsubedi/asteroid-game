550c7fc956ea9bf43bcdf5cf45f32e8c556daa36
atul subedi <atul.work15@gmail.com>
2024-08-26 16:17:05 +0545

    score board added

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from shot import Shot
 
 def main():
     pygame.init()
+    pygame.font.init()
+    score = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     b = pygame.time.Clock()
     dt = 0
@@ -17,8 +19,6 @@ def main():
     shot = pygame.sprite.Group()
     Shot.containers = (shot, updatable, drawable)
 
-
-
     Player.containers = (updatable, drawable)
     p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
     Asteroid.containers = (asteroid, drawable, updatable)
@@ -26,6 +26,7 @@ def main():
     asf = AsteroidField()
 
     while True:
+        font = pygame.font.Font(None, 36)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -38,16 +39,17 @@ def main():
                 sys.exit()
             for s in shot:
                 if obj.check_collision(s):
-                 s.kill()
-                 obj.split()
-
+                    score += 10
+                    s.kill()
+                    obj.split()
 
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
+        score_text = font.render(f'Score: {score}', True, (255, 255, 255))
+        screen.blit(score_text, (10, 10))
         pygame.display.flip()
         dt = b.tick(60) / 1000
-
 
 
 if __name__ == "__main__":
